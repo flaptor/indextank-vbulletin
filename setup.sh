5a09ed4d0465ff33394bb7ef6f121c09f7a3b9ba
leandro <loniszczuk@gmail.com>
2011-02-08 18:52:54 -0800

    Add batch index of all documents to the indextank_plugin.php script. Need to change the package config because of database configuration.

--- setup.sh
+++ setup.sh
@@ -6,23 +6,12 @@ read API_URL
 echo "Enter your index name:"
 read INDEX_NAME
 
-# set up php files
-if [ ! -e "indextank" ]
-then
-    mkdir indextank
-fi
-
 # copy files to a set up dir, just to avoid touching the original ones
-cp indextank_nosetup/indextank_plugin.php indextank/indextank_plugin.php
-cp indextank_nosetup/indextank_client.php indextank/indextank_client.php
+cp indextank_plugin.php.new indextank_plugin.php
 
-cd indextank
-
-sed "s|<API_URL>|$API_URL|" indextank_plugin.php > indextank_plugin.php.1
-sed "s|<INDEX_NAME>|$INDEX_NAME|" indextank_plugin.php.1 > indextank_plugin.php
-rm indextank_plugin.php.1
-
-cd ..
+sed "s|<API_URL>|$API_URL|" indextank_plugin.php > indextank_plugin.php.tmp
+sed "s|<INDEX_NAME>|$INDEX_NAME|" indextank_plugin.php.tmp > indextank_plugin.php
+rm indextank_plugin.php.tmp
 
 echo 'Setup successful'
 
